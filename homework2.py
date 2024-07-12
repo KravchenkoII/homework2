d36867c449bbcea472c63258480e5ed30dc7f8e3
KravchenkoII <group777@mail.ru>
2024-07-12 23:16:34 +0300

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -1,6 +1,7 @@
 Count_DZ=12
 Count_Time=1.5
 Name_Courses='Python'
-Time_One_DZ=1.5/12
+# Time_One_DZ=1.5/12
+Time_One_DZ=Count_Time/Count_DZ
 print('Курс:', Name_Courses+',', 'всего задач:', Count_DZ, 'затрачено часов:', Count_Time, 'среднее время выполнения', Time_One_DZ,
       'часа.')
